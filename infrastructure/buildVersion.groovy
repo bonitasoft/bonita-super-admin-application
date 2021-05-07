120f67f0ea0eae093211e59ad6d3cf234d87d8e1
abirembaut <anthony.birembaut@gmail.com>
2021-05-07 11:48:58 +0200

    feat(super admin app): create application with all artifacts
    Covers [RUNTIME-184](https://bonitasoft.atlassian.net/browse/RUNTIME-184)

---
+++ infrastructure/buildVersion.groovy
@@ -0,0 +1,17 @@
+pipeline {
+    agent any
+    options {
+        timestamps()
+    }
+    stages {
+        stage('Build and deploy') {
+            steps {
+                sh "./mvnw deploy -DskipTests -DaltDeploymentRepository=${env.ALT_DEPLOYMENT_REPOSITORY_TAG}"
+                withCredentials([string(credentialsId: 'github-api', variable: 'GITHUB_API_TOKEN')]) {
+                    sh "./infrastructure/upload-github-release-asset.sh github_api_token=$GITHUB_API_TOKEN tag=${params.version} filename=./community/target/bonita-super-admin-application-${params.version}.bos"
+                    sh "./infrastructure/upload-github-release-asset.sh github_api_token=$GITHUB_API_TOKEN tag=${params.version} filename=./subscription/target/bonita-super-admin-application-sp-${params.version}.bos"
+                }
+            }
+        }
+    }
+}
